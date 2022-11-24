a514fa4c75b06c0fde46301124f4cda48ad2187c
annezdz <annezdz@hotmail.com>
2022-11-24 16:56:03 -0300

    adding logger

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -1,7 +1,12 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
 from utilities import configReader
+import logging
+from utilities.LogUtil import Logger
+
+log = Logger(__name__, logging.INFO)
 
 
 class BasePage:
@@ -19,6 +24,7 @@ class BasePage:
         elif str(locator).endswith('_ID'):
             self.driver.find_element(By.ID, configReader.read_config
             ("locators", locator)).click()
+        log.logger.info("Clicking on an element: " + str(locator))
 
     def type(self, locator, value):
         if str(locator).endswith('_XPATH'):
@@ -30,17 +36,42 @@ class BasePage:
         elif str(locator).endswith('_ID'):
             self.driver.find_element(By.ID, configReader.read_config
             ("locators", locator)).send_keys(value)
+        log.logger.info("Typing on an element: " + str(locator) +
+                        " value entered as: " + str(value))
 
     def select(self, locator, value):
         global dropdown
         if str(locator).endswith('_XPATH'):
-            dropdown = self.driver.find_element(By.XPATH, configReader.read_config
-            ("locators", locator))
+            dropdown = self.driver.find_element(By.XPATH,
+                                                configReader.read_config
+                                                ("locators", locator))
         elif str(locator).endswith('_CSS'):
-            dropdown = self.driver.find_element(By.CSS_SELECTOR, configReader.read_config
-            ("locators", locator))
+            dropdown = self.driver.find_element(By.CSS_SELECTOR,
+                                                configReader.read_config
+                                                ("locators", locator))
         elif str(locator).endswith('_ID'):
             dropdown = self.driver.find_element(By.ID, configReader.read_config
             ("locators", locator))
         select = Select(dropdown)
         select.select_by_visible_text(value)
+        log.logger.info("Selecting on an element: " + str(locator) +
+                        " and select value " + str(value))
+
+    def moveTo(self, locator, value):
+        global element
+        if str(locator).endswith('_XPATH'):
+            element = self.driver.find_element(By.XPATH,
+                                               configReader.read_config
+                                               ("locators", locator))
+        elif str(locator).endswith('_CSS'):
+            element = self.driver.find_element(By.CSS_SELECTOR,
+                                               configReader.read_config
+                                               ("locators", locator))
+        elif str(locator).endswith('_ID'):
+            element = self.driver.find_element(By.ID, configReader.read_config
+            ("locators", locator))
+
+        action = ActionChains(self.driver)
+        action.move_to_element(element).perform()
+        log.logger.info("Moving to an element: " + str(locator) +
+                        " and select value " + str(value))
--- testcases/test_signup.py
+++ testcases/test_signup.py
@@ -4,6 +4,11 @@ from pages.RegistrationPage import RegistrationPage
 from testcases.BaseTest import BaseTest
 from utilities.dataProvider import get_data
 
+import logging
+from utilities.LogUtil import Logger
+
+log = Logger(__name__, logging.INFO)
+
 
 class Test_SignUp(BaseTest):
 
@@ -12,6 +17,9 @@ class Test_SignUp(BaseTest):
         get_data('LoginTest'))
     def test_do_signup(self, name, phone_number, email, country, city,
                        username, password):
+        log.logger.info('Test do sign started')
         reg_page = RegistrationPage(self.driver)
         reg_page.fill_form(name, phone_number, email, country, city, username,
                            password)
+        log.logger.info('Test do sign successfully executed')
+
